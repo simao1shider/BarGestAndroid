9a32715f322e234c105b8d2bd3245df22400f956
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-03 15:29:12 +0000

    gitflow-feature-stash: Layout_Login

--- BarGest/app/src/main/java/com/example/bargest/Views/LoginActivity.java
+++ BarGest/app/src/main/java/com/example/bargest/Views/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.bargest.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bargest.R;
 
@@ -12,8 +14,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void ValidateLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //TODO:Transition for main Layout
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
 
 
-        
+    }
+
+
+    private boolean IsValidUsername(){
+        return true;
+    }
+
+    private boolean IsValidPassword(){
+        return true;
     }
 }
