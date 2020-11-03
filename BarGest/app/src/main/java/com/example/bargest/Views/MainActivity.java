ebae4ef58a419f3f6def9ad288bba9e26b6f2276
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-03 15:43:50 +0000

    PROJ2021-200 #ready-for-testing Finalização do layout

--- BarGest/app/src/main/java/com/example/bargest/Views/MainActivity.java
+++ BarGest/app/src/main/java/com/example/bargest/Views/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.bargest.Views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.bargest.R;
+import com.example.bargest.Views.Fragments.HomeFragment;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ChipNavigationBar navigationBar = findViewById(R.id.bottom_bar);
+
+        navigationBar.setItemSelected(R.id.home,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment()).commit();
+
+
+    }
+
+    public void changeFragment(View view) {
+
     }
 }
