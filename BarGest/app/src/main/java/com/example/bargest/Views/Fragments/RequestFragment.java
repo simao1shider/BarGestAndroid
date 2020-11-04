1b1c8f153e34909ed347aa991b3b9329ff65d179
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-04 00:26:21 +0000

    PROJ2021-187 #start Inicio da aplicação

---
+++ BarGest/app/src/main/java/com/example/bargest/Views/Fragments/RequestFragment.java
@@ -0,0 +1,32 @@
+package com.example.bargest.Views.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bargest.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RequestFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RequestFragment extends Fragment {
+    
+
+    public RequestFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_request, container, false);
+    }
+}
