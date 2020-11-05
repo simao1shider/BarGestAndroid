098dba2547d2291b2744f757078dd924ddadceef
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-05 19:27:39 +0000

    gitflow-feature-stash: PROJ2021-189-layout-de-pesquisa-de-produ

---
+++ BarGest/app/src/main/java/com/example/bargest/Views/Fragments/NewRequestFragment.java
@@ -0,0 +1,29 @@
+package com.example.bargest.Views.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bargest.R;
+
+
+public class NewRequestFragment extends Fragment {
+
+
+    public NewRequestFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+         View view = inflater.inflate(R.layout.fragment_new_request, container, false);
+
+        return view;
+    }
+}
