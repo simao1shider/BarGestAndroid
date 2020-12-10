9bdab7cd4d0ededaa00688fc7e43b4bbc80642fd
Simão Marques <simao.shider@gmail.com>
2020-12-10 16:21:13 +0000

    #PROJ2021-169 #ready-for-testing

--- BarGest/app/src/main/java/com/example/bargest/Views/Fragments/HomeFragment.java
+++ BarGest/app/src/main/java/com/example/bargest/Views/Fragments/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.bargest.R;
 
@@ -61,6 +62,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageButton categoriesbutton = view.findViewById(R.id.categoriesButton);
+        categoriesbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.container, new ListCategoriesFragment()).commit();
+            }
+        });
+        return view;
     }
 }
