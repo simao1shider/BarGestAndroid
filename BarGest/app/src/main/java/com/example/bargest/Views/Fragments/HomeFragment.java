52367163d05fb626d0e9211aab8a12d5caf7b906
Yaroslav Antonenko <ysik345@gmail.com>
2021-01-18 22:39:34 +0000

    Logout

--- BarGest/app/src/main/java/com/example/bargest/Views/Fragments/HomeFragment.java
+++ BarGest/app/src/main/java/com/example/bargest/Views/Fragments/HomeFragment.java
@@ -4,17 +4,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bargest.R;
 import com.example.bargest.SingletonBarGest;
 import com.example.bargest.Views.LoginActivity;
+import com.example.bargest.Views.MainActivity;
 
 import java.util.Date;
 
@@ -27,59 +30,35 @@ import static android.content.Context.MODE_PRIVATE;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     TextView textView6;
-
+    ImageView BtnLogout;
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        SharedPreferences prefs = getActivity().getSharedPreferences("Pref", MODE_PRIVATE);
+        final SharedPreferences prefs = getActivity().getSharedPreferences("Pref", MODE_PRIVATE);
         if(prefs.getString("token","")==""){
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
         }
         ImageButton categoriesbutton = view.findViewById(R.id.categoriesButton);
+        BtnLogout = view.findViewById(R.id.BTNlogout);
+
+        BtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prefs.edit().remove("token");
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         categoriesbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
